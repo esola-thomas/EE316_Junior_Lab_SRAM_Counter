00e257eedea376126dd95ae12e024ea64f44da0d
Ernesto Sola-Thomas <ernesto@solathomas.com>
2023-01-18 19:01:53 -0500

    Kaili latest counter source code

--- counter.vhd
+++ counter.vhd
@@ -1,62 +1,43 @@
 library IEEE;
-
 use IEEE.STD_LOGIC_1164.ALL;
-
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-entity counter is 
+entity counter is
+	generic ( constant CNT_MAX: std_logic_vector(7 downto 0) := X"0F");
+	port (  
+		clk : in std_logic;
+		direction : in std_logic;
+		halt : in std_logic;
+		ireset : in std_logic;
+		cout : out std_logic_vector(7 downto 0));
+end entity;
 
-	generic ( CNT_MAX 	:= X”0000000F”);
+architecture behavioral of counter is
+	
+	-- Signals
+	signal cnt : std_logic_vector(7 downto 0) := (others => '0');
+	begin
 
-	port (  clk			: in std_logic;
-
-		direction		: in std_logic;
-
-		halt			: in std_logic;
-
-		ireset 			: in std_logic; 
-
-
-
-		cout			: out std_logic_vector(7 downto 0);
-
-	) 
-
-end entity; 
-
-architecture behavioral of counter is 
-
-	signal cnt 		: std_logic_vector(7 downto 0) := (others => ‘0’);
-
-begin 
-
-	process(clk) begin
-
-		if (cnt = CNT_MAX) then
-
-			cnt <= (others => ‘0’);
-
-		if (ireset = ‘1’) then
-
-			cnt <= (others => ‘0’);
-
-		if rising_edge(clk) then
-
-			if(direction = ‘1’) then 
-
-				cnt <= cnt + 1;
-
-			else 
-
-				cnt <= cnt - 1;
-
-		end if;
-
-	end if;
-
-	end process; 
+	process(clk, ireset)
+		begin
+			if (rising_edge(clk)) then
+				if (ireset = '1') then
+					cnt <= (others => '0');
+				elsif(direction = '1') then
+					if (cnt = 0) then
+					cnt <= CNT_MAX;
+					else
+					cnt <= cnt - 1;
+					end if;
+				else
+					if (cnt = CNT_MAX) then
+						cnt <= (others => '0');
+					else
+						cnt <= cnt + 1;
+					end if;
+				end if;
+			end if;
+	end process;
 
 	cout <= cnt;
-
 end behavioral;
-
